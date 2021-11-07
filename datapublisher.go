88a4e13180dd6b9a103f6887213948a23df4f885
Shubham <dev4fun007@gmail.com>
2021-11-07 17:11:55 +0530

    initial commit for the data publisher

---
+++ datapublisher.go
@@ -0,0 +1,58 @@
+package publisher
+
+import (
+	"context"
+	"github.com/dev4fun007/autobot-common"
+	"github.com/rs/zerolog/log"
+)
+
+const (
+	QueueBufferSize  = 8
+	DataPublisherTag = "TickerDataPublisher"
+)
+
+type TickerDataPublisher struct {
+	registry        common.WorkerRegistryService // get all active worker data channel to send data to
+	marketWorkerMap map[string]common.Worker
+	tickerDataChan  chan []common.TickerData // use this READ-ONLY channel to read the ticker data received from broker
+}
+
+func NewDataPublisher(registry common.WorkerRegistryService) TickerDataPublisher {
+	return TickerDataPublisher{
+		registry:       registry,
+		tickerDataChan: make(chan []common.TickerData, QueueBufferSize),
+	}
+}
+
+func (receiver TickerDataPublisher) Publish(tickers []common.TickerData) {
+	receiver.tickerDataChan <- tickers
+}
+
+func (receiver TickerDataPublisher) StartFanOutService(ctx context.Context) {
+	log.Info().Str(common.LogComponent, DataPublisherTag).
+		Msg("starting data fan out service")
+	go func() {
+		for {
+			select {
+			case data := <-receiver.tickerDataChan:
+				{
+					marketWorkerMap := receiver.registry.GetActiveWorkers()
+					log.Debug().Str(common.LogComponent, DataPublisherTag).Int("worker-count", len(marketWorkerMap)).Msg("fetched all active workers")
+					for _, tickerData := range data {
+						if val, ok := marketWorkerMap[tickerData.Market]; ok {
+							for _, worker := range val {
+								worker.GetDataChannel() <- tickerData
+							}
+						}
+					}
+				}
+			case <-ctx.Done():
+				{
+					log.Info().Str(common.LogComponent, DataPublisherTag).
+						Msg("stopping data listener and publisher")
+					return
+				}
+			}
+		}
+	}()
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/dev4fun007/autobot-data-publisher
+
+go 1.17
+
+require (
+	github.com/dev4fun007/autobot-common v0.0.0-20211107113013-ec988f958828
+	github.com/rs/zerolog v1.26.0
+)
